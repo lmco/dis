456d4a19716676cbdb9f7bc8f8089507a9dfe072
McDaniel, Donte S <donte.s.mcdaniel@lmco.com>
2020-11-20 09:15:00 -0500

    Updated Publisher and subscriber

--- pubsub/publisher.js
+++ pubsub/publisher.js
@@ -2,12 +2,29 @@
 
 // Import Redis
 const Redis = require('ioredis');
+const logger = require('../lib/logger');
 
 // Publisher Instance
 const publisher = new Redis({
     port: process.env.REDIS_PORT, // Redis port
     host: process.env.REDIS_HOST, // Redis host
     db: process.env.REDIS_DB
+});
+
+publisher.on('connect', () => {
+    logger.info('Redis Publisher successfully connected');
+});
+
+publisher.on('end', () => {
+    logger.err('Redis disconnected');
+});
+
+publisher.on('error', (err) => {
+    logger.err('Redis error: ', err);
+});
+
+publisher.on('reconnecting', () => {
+    logger.info('Reconnecting Redis');
 });
 
 // Exporting the publisher
--- pubsub/subscriber.js
+++ pubsub/subscriber.js
@@ -2,12 +2,29 @@
 
 // Import Redis
 const Redis = require('ioredis');
+const logger = require('../lib/logger');
 
 // Subscriber Instance
 const subscriber = new Redis({
     port: process.env.REDIS_PORT, // Redis port
     host: process.env.REDIS_HOST, // Redis host
     db: process.env.REDIS_DB
+});
+
+subscriber.on('connect', () => {
+    logger.info('Redis Subscriber successfully connected');
+});
+
+subscriber.on('end', () => {
+    logger.err('Redis disconnected');
+});
+
+subscriber.on('error', (err) => {
+    logger.err('Redis error: ', err);
+});
+
+subscriber.on('reconnecting', () => {
+    logger.info('Reconnecting Redis');
 });
 
 // Exporting the subscriber
